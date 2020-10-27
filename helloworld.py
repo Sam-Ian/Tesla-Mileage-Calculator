1e0afaf1b59201d75cfe993edb2c4f3afc77f6fd
Ian May <73487798+ianmay231@users.noreply.github.com>
2020-10-27 19:40:10 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,10 @@ print("Hello World")
 print("Hi Sam, I added this bit of code")
 print("Can you see it?")
 print("Ian is now collaborating on two different computers!")
+print("1")
+print("2")
+print("3")
+
 print("Another line of code added!")
 
 
