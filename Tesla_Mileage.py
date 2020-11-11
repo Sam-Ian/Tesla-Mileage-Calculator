839ea56df844644bfa49388140b6d7805e48ad83
Sam Thompson <samthompson292@gmail.com>
2020-11-11 17:34:15 +0000

    debugging csv read

--- Tesla_Mileage.py
+++ Tesla_Mileage.py
@@ -1,34 +1,24 @@
 import csv
 
 
-with open('TeslaMileage.csv', newline='') as tesla_mileage:
+with open('tesla_changed_dates.csv', newline='') as tesla_mileage:
     read_tesla_miles = csv.DictReader(tesla_mileage)
 
-    list_of_dates = []
+    list_of_fred = []
     list_of_mileage = []
     list_of_mileage_in_period = []
-    list_of_elapsed_days = []
-
-
-    #WHY IS THERE A KEY ERROR WITH THIS FOR LOOP, 'Date' IS THE CORRECT KEY IN THE CSV FILE?!?!
-    for row in read_tesla_miles:
-        list_of_dates.append(row['Date'])
-
-    #ONLY THE ABOVE FOR LOOP WORKS^^^
-    #TOP FOR LOOP IS THE ONLY ONE THAT EXECUTES WHICHEVER ONE I PUT AT THE TOP
+    list_of_elapsed_days = []  
 
     for row in read_tesla_miles:
-        list_of_mileage.append(row['Mileage'])
-
-    for row in read_tesla_miles:
+        list_of_fred.append(row['Fred']) 
         list_of_mileage_in_period.append(row['Mileage in period'])
-        
-    for row in read_tesla_miles:
+        list_of_mileage.append(row['Mileage'])
         list_of_elapsed_days.append(row['Elapsed days'])
 
-
+print(list_of_fred)
 print(list_of_mileage)
 print(list_of_mileage_in_period)
+print(list_of_elapsed_days)
    
 
 
