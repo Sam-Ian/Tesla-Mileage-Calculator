ed3119b594ab11ff8cc52255f3f6c0ee0f20f066
Sam Thompson <samthompson292@gmail.com>
2020-11-11 18:08:40 +0000

    a

--- Tesla_Mileage.py
+++ Tesla_Mileage.py
@@ -1,7 +1,7 @@
 import csv
 
 
-with open('TeslaMileageSam.csv', newline='') as tesla_mileage:
+with open('TeslaMileageEdit.csv', newline='') as tesla_mileage:
     read_tesla_miles = csv.DictReader(tesla_mileage)
 
     list_of_fred = []
@@ -10,7 +10,7 @@ with open('TeslaMileageSam.csv', newline='') as tesla_mileage:
     list_of_elapsed_days = []  
 
     for row in read_tesla_miles:
-        list_of_fred.append(row['Fred']) 
+        list_of_fred.append(row['Sam']) 
         list_of_mileage_in_period.append(row['Mileage in period'])
         list_of_mileage.append(row['Mileage'])
         list_of_elapsed_days.append(row['Elapsed days'])
@@ -21,5 +21,4 @@ print(list_of_mileage_in_period)
 print(list_of_elapsed_days)
    
 
-
-    
+ 
