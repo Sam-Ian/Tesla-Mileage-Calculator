e2c61fb02a8f49fed19e0b4d159b98eeb80157bd
Sam Thompson <samthompson292@gmail.com>
2020-11-11 16:22:49 +0000

    Added error info

--- Tesla_Mileage.py
+++ Tesla_Mileage.py
@@ -9,8 +9,13 @@ with open('TeslaMileage.csv', newline='') as tesla_mileage:
     list_of_mileage_in_period = []
     list_of_elapsed_days = []
 
+
+    #WHY IS THERE A KEY ERROR WITH THIS FOR LOOP, 'Date' IS THE CORRECT KEY IN THE CSV FILE?!?!
     for row in read_tesla_miles:
         list_of_dates.append(row['Date'])
+
+    #ONLY THE ABOVE FOR LOOP WORKS^^^
+    #TOP FOR LOOP IS THE ONLY ONE THAT EXECUTES WHICHEVER ONE I PUT AT THE TOP
 
     for row in read_tesla_miles:
         list_of_mileage.append(row['Mileage'])
@@ -22,7 +27,8 @@ with open('TeslaMileage.csv', newline='') as tesla_mileage:
         list_of_elapsed_days.append(row['Elapsed days'])
 
 
-
+print(list_of_mileage)
+print(list_of_mileage_in_period)
    
 
 
